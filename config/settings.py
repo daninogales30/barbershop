b90f47ab307a2defbb31e6402cce68a8402b69b8
Dani <dnogmat3011@g.educaand.es>
2026-05-22 19:37:01 +0200

    Proyecto desplegado

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,11 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "barbershop-utjf.onrender.com",
+    "localhost",
+    "127.0.0.1"
+]
 
 # Application definition
 
