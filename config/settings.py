f856c35a1ce76c6e7bfbef689e866deebab28cad
Dani <dnogmat3011@g.educaand.es>
2026-05-22 19:18:16 +0200

    .

--- config/settings.py
+++ config/settings.py
@@ -77,7 +77,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3'
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
