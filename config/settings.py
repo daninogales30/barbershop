d3a3840f08b1cf17e1bd38a42828e1df13e26b32
Dani <dnogmat3011@g.educaand.es>
2026-05-25 15:52:30 +0200

    Cambios en el settings arreglando form de contacto

--- config/settings.py
+++ config/settings.py
@@ -130,9 +130,11 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 
-EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
+
+# EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = 'danixdxdfortnite@gmail.com'
 
